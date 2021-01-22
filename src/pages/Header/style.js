64fcc5797e53efdeee20828ae7919a5316ac8503
Lucas Goncalves <lucas.goncalves@oystr.com.br>
2021-01-22 09:55:25 -0300

    menu superior

--- src/pages/Header/style.js
+++ src/pages/Header/style.js
@@ -7,27 +7,153 @@ export const Header = styled.header`
   background-repeat: no-repeat;
   background-position: right;
   background-size: cover;
+  
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  padding-top: 100px;
+  
   font-size: calc(10px + 2vmin);
   color: white;
   text-align: center;
-  div{
+  
+  height: 500px;
+
+  > div{
     width: 70vw;
-  }
-  @media (min-width: 360px){
-    div{
+    
+    @media (min-width: 360px){
       width: 90vw;
     }
+    
+    h1{
+      font-family: 'AvenirBold';
+    }
+  
+    p{
+      font-family: 'AvenirRegular-500';
+      margin: auto;
+    }
   }
-  h1{
-    font-family: 'AvenirBold';
+  
+  
+`;
+
+export const NavBar = styled.div`
+  position: fixed;
+  z-index: 2;
+  background: var(--secondary);
+
+  padding: 0 3rem;
+
+  width: 100%;
+  display: flex;
+  align-items: center;
+  
+  .logo {
+    width: 200px;
+    margin: 1.5rem 0;
+
+    @media(min-width:1000px) {
+      width: initial;
+    }
   }
 
-  p{
-    font-family: 'AvenirRegular-500';
-    margin: auto;
+  ul {
+    display: none;
+    
+    list-style: none;
+    margin-left: auto;
+    margin-right: 100px;
+  
+    @media(min-width:1000px) {
+      display: flex;
+    }
+    
+    li {
+      margin: 0 1.5rem;
+
+      a {
+        cursor: pointer;
+
+        font-size: 14px;
+        font-weight: 700;
+        color: var(--white);
+  
+        &:hover {
+          color: var(--white);
+          opacity: 0.8;
+          
+        }
+      }
+
+      ul {
+        display: none;
+        position: absolute; 
+      
+        width: 300px; 
+        background: var(--white); 
+        padding: 1.5rem !important;
+      
+        border-radius: 4px;
+
+        li {
+          margin: 0.5rem 0 !important;
+        }
+      }
+
+      &:hover {
+        ul {
+          display: flex;
+        }
+      }
+    }
   }
+
+ .menu {
+    margin-left: auto;
+
+
+    @media(min-width: 1000px) {
+      display: none;
+    }
+  }
+`;
+
+export const MenuMobile = styled.ul`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    display: ${props => props.toggle ? 'flex' : 'none'};
+    background: var(--secondary);
+    
+    list-style: none;
+    padding-top: 100px;
+    z-index: 3;
+
+    @media(min-width: 1000px) {
+      display: none;
+    }
+    
+    li {
+      margin: 0 1.5rem;
+
+      a {
+        cursor: pointer;
+
+        font-size: 14px;
+        font-weight: 700;
+        color: var(--white);
+
+        &:hover {
+          color: var(--white);
+          opacity: 0.8;
+        }
+      }
+    }
 `;
